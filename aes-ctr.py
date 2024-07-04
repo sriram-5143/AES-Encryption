450149d6cff567341ca651e984db02789c3b8083
Sriram Kotha <80691996+sriram-5143@users.noreply.github.com>
2024-07-04 11:35:03 +0530

    Aes-ctr.py

--- aes-ctr.py
+++ aes-ctr.py
@@ -1,28 +1,19 @@
-# AES cryptography algorithm implementation in Counter mode
-
-
-
+import argparse
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
-import sys, argparse
 
-VERSION = "aes-ctr.py 0.1"
+VERSION = "aes-ctr.py 0.2"
 
 def main():
     cmd = argparse.ArgumentParser(
-	    prog="aes-ctr.py",
-	    formatter_class=argparse.RawDescriptionHelpFormatter,
-	    description = '''
-	    \rAES implementation in counter mode. This version supports 128 bits key encryption only.
-	    \rThis is experimental script. NO INPUT VALIDATION
-	    ''',
-	    epilog = '''
-	    \rExemplary usage:\n
-	    \r1) Encryption
-	    \r$ python aes-ctr.py -i plaintext -o ciphertext -k abcdef1234567890abcdef1234567890 -iv 01010101010101010101010101010101\n
-	    \r2) Decryption
-	    \r$ aes-ctr.py -d -i ciphertext -o plaintext -k abcdef1234567890abcdef1234567890 -iv 01010101010101010101010101010101
-	    ''')
+        prog="aes-ctr.py",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='''
+        AES implementation in counter mode. This version supports 128 bits key encryption only.
+        This is an experimental script. NO INPUT VALIDATION
+        ''',
+        epilog=
+        
 
     cmd.add_argument("-d", "--decrypt", help="Use decrypt instead of default encrypt", action="store_true")
     cmd.add_argument("-i", "--input", help="File containing plaintext/ciphertext", type=str, required=True, metavar="IN")
@@ -32,78 +23,74 @@ def main():
     cmd.add_argument("-v", "--version", action="version", version=VERSION)
     args = cmd.parse_args()
 
-    # Retrieve options and arguments # 
-    if args.output:
-	OUT_FILE = args.output
-    else:
-	OUT_FILE = ''
-
+    # Retrieve options and arguments 
+    OUT_FILE = args.output if args.output else ''
     IN_FILE = args.input
 
-    KEY = validateHex(args.key)
-    IV = validateHex(args.iv)
+    KEY = validate_hex(args.key)
+    IV = validate_hex(args.iv)
 
-    # Read file that contains plaintext / ciphertext #  
-    f = open(IN_FILE, 'rb')
-    text = f.read()
-    f.close()
+    # Read file that contains plaintext / ciphertext 
+    try:
+        with open(IN_FILE, 'rb') as f:
+            text = f.read()
+    except FileNotFoundError:
+        print(f"File {IN_FILE} not found.")
+        sys.exit(1)
 
-    # If key or init counter didn't pass validation test then abort #
+    # If key or init counter didn't pass validation test then abort 
     if KEY and IV:
-	# Decryption #
-	if args.decrypt:
-	    crypto(text.encode("hex"), KEY, IV, OUT_FILE, encrypt=False)
-	# Encryption #
-	else:
-	    crypto(text.encode("hex"), KEY, IV, OUT_FILE)
+        text_hex = text.hex()
+        # Decryption 
+        if args.decrypt:
+            crypto(text_hex, KEY, IV, OUT_FILE, encrypt=False)
+        # Encryption 
+        else:
+            crypto(text_hex, KEY, IV, OUT_FILE)
     else:
-	print "Invalid Key or iv"
+        print("Invalid Key or IV")
 
-# Validate if passed value is hexadecimal and has proper length #
-# Function returns passed argument if value is correct #
-# If passed value is not valid, function returns False #
-def validateHex(hex):
-    if len(hex) is not 32:
-	return False
-    else:
-	try:
-	    int(hex,16)
-	    return hex
-	except ValueError:
-	    return False
+# Validate if passed value is hexadecimal and has proper length 
+# Function returns passed argument if value is correct 
+# If passed value is not valid, function returns False 
+def validate_hex(hex_str):
+    if len(hex_str) != 32:
+        return False
+    try:
+        int(hex_str, 16)
+        return hex_str
+    except ValueError:
+        return False
 
-# Core function that performs encryption / decryption #
+# Core function that performs encryption / decryption 
 def crypto(text, key, iv, output, encrypt=True):
+    BlockSizeForHex = 32
+    encryption_key = bytes.fromhex(key)
 
-    BlockSizeForHex = 32;
-    encryptionKey = key.decode("hex")
+    # Create new Counter object 
+    counter = Counter.new(128, initial_value=int(iv, 16))
 
-    # Create new Counter object #
-    # Object will automatically increment counter on each cryptographic round #
-    counter = Counter.new(128, initial_value=int(iv,16))
+    # Create new AES CTR object 
+    cipher = AES.new(encryption_key, AES.MODE_CTR, counter=counter)
 
-    # Create new AES CTR object #
-    cipher=AES.new(encryptionKey, AES.MODE_CTR, counter=counter)
+    result = b''
 
-    result = ''
-
-    # Iterate over text #
-    for i in range(0,len(text),BlockSizeForHex):
-
-	# AES CTR operates on 16 bytes blocks #
-	block = text[i:i+BlockSizeForHex]
-	if encrypt:
-	    result += cipher.encrypt(block.decode("hex"))
-	else:
-	    result += cipher.decrypt(block.decode("hex"))
+    # Iterate over text 
+    for i in range(0, len(text), BlockSizeForHex):
+        # AES CTR operates on 16 bytes blocks 
+        block = text[i:i+BlockSizeForHex]
+        block_bytes = bytes.fromhex(block)
+        if encrypt:
+            result += cipher.encrypt(block_bytes)
+        else:
+            result += cipher.decrypt(block_bytes)
 
     if output:
-	f = open(output,'wb')
-	f.write(result)
-	f.close()
+        with open(output, 'wb') as f:
+            f.write(result)
     else:
-	print result
-
+        print(result)
 
 if __name__ == '__main__':
     main()
+
